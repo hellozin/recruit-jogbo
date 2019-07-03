e08889e0de3fe5f108206620062b24bed1cc0f48
hellozin <paul26375@gmail.com>
2019-07-03 16:36:46 +0900

    Initial commit

---
+++ src/main/java/univ/study/recruitjogbo/RecruitJogboApplication.java
@@ -0,0 +1,13 @@
+package univ.study.recruitjogbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitJogboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitJogboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/univ/study/recruitjogbo/RecruitJogboApplicationTests.java
@@ -0,0 +1,16 @@
+package univ.study.recruitjogbo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruitJogboApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

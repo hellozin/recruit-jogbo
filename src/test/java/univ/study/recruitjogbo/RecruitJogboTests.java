dfe385bb1976358361bd64c9bb7c52f5f4a028eb
hellozin <paul26375@gmail.com>
2019-07-06 04:18:15 +0900

    REFACTOR: Runner 이름 변경

--- src/test/java/univ/study/recruitjogbo/RecruitJogboTests.java
+++ src/test/java/univ/study/recruitjogbo/RecruitJogboTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RecruitJogboApplicationTests {
+public class RecruitJogboTests {
 
     @Test
     public void contextLoads() {

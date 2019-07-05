74a80a05f793839b4361d5721606981e1340f622
hellozin <paul26375@gmail.com>
2019-07-06 04:18:04 +0900

    REFACTOR: Runner 이름 변경

--- src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
+++ src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecruitJogboApplication {
+public class RecruitJogbo {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecruitJogboApplication.class, args);
+        SpringApplication.run(RecruitJogbo.class, args);
     }
 
 }

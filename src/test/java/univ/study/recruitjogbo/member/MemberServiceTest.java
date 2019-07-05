c25b0f50343e1047dfa81d4d3b21d94759b88817
hellozin <paul26375@gmail.com>
2019-07-06 04:13:49 +0900

    ADD: CSRF, Headers 설정 및 모든 요청 허용

---
+++ src/test/java/univ/study/recruitjogbo/member/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package univ.study.recruitjogbo.member;
+
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MemberServiceTest {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MemberService memberService;
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    @BeforeAll
+    void setUp() {
+        userId = "hellozin";
+        password = "1234";
+        name = "hello";
+        email = "hello@ynu.ac.kr";
+    }
+
+    @Test
+    @Order(1)
+    void 사용자를_추가한다() {
+        Member member = memberService.join(userId, password, name, email);
+        assertThat(member, is(notNullValue()));
+        assertThat(member.getSeq(), is(notNullValue()));
+        assertThat(member.getEmail(), is(email));
+        log.info("Join member : {}", member);
+    }
+
+    @Test
+    @Order(2)
+    void 사용자_목록을_가져온다() {
+        List<Member> members = memberService.findAll();
+        assertThat(members, is(notNullValue()));
+        assertThat(members.size(), is(1));
+    }
+
+}
---
+++ src/test/java/univ/study/recruitjogbo/post/PostServiceTest.java
@@ -0,0 +1,59 @@
+package univ.study.recruitjogbo.post;
+
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import univ.study.recruitjogbo.member.RecruitType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PostServiceTest {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private PostService postService;
+
+    private String companyName;
+    private RecruitType recruitType;
+    private LocalDate deadLine;
+    private String review;
+
+    @BeforeAll
+    void setUp() {
+        companyName = "hellozin";
+        recruitType = RecruitType.RESUME;
+        deadLine = LocalDate.now();
+        review = "review";
+    }
+
+    @Test
+    @Order(1)
+    void 포스트를_작성한다() {
+        Post post = postService.write(companyName, recruitType, deadLine, review);
+        assertThat(post, is(notNullValue()));
+        assertThat(post.getSeq(), is(notNullValue()));
+        assertThat(post.getRecruitType(), is(recruitType));
+        assertThat(post.getDeadLine(), is(deadLine));
+        log.info("Written post : {}", post);
+    }
+
+    @Test
+    @Order(2)
+    void 사용자_목록을_가져온다() {
+        List<Post> posts = postService.findAll();
+        assertThat(posts, is(notNullValue()));
+        assertThat(posts.size(), is(1));
+    }
+
+}

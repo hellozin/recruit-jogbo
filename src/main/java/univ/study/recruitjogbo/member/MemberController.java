6e7ade83e6702ad7f4cfbde1be0d5de507df67ed
hellozin <paul26375@gmail.com>
2019-07-06 17:23:51 +0900

    UPDATE: url에 api/ 추가

--- src/main/java/univ/study/recruitjogbo/member/MemberController.java
+++ src/main/java/univ/study/recruitjogbo/member/MemberController.java
@@ -1,15 +1,13 @@
 package univ.study.recruitjogbo.member;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import univ.study.recruitjogbo.request.JoinRequest;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;

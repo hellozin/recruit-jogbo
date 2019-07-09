1890dac4e101f784fd2dfe06e4b62b2fffd240a4
hellozin <paul26375@gmail.com>
2019-07-10 01:11:30 +0900

    ADD: OAuth 설정 추가

--- src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
+++ src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
@@ -1,13 +1,20 @@
 package univ.study.recruitjogbo.configure;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    private final Filter ssoFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,7 +24,8 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().headers().frameOptions().sameOrigin()
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class);
     }
 
 }

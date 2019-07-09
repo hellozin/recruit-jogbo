0404d832dbdfdd66a75b2e587334cf9191fa64aa
hellozin <paul26375@gmail.com>
2019-07-10 00:47:43 +0900

    TEST: '/' url 접근 테스트 작성

--- src/test/java/univ/study/recruitjogbo/RecruitJogboTests.java
+++ src/test/java/univ/study/recruitjogbo/RecruitJogboTests.java
@@ -1,16 +1,33 @@
 package univ.study.recruitjogbo;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RecruitJogboTests {
 
+    @Before
+    public void setup() {
+        RestAssured.port = 8080;
+    }
+
     @Test
-    public void contextLoads() {
+    public void 기본_path로_접근하면_index_html이_호출된다() throws Exception {
+        given()
+                .when()
+                    .get("/")
+                .then()
+                    .statusCode(200)
+                    .contentType("text/html")
+                    .body(containsString("권한 관리"));
     }
 
 }

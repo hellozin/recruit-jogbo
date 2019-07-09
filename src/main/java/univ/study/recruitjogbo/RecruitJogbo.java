7e56f2a5d043ec8ad87ca3cf252eda838005a4d9
hellozin <paul26375@gmail.com>
2019-07-09 22:54:27 +0900

    CONFIG: 구글 OAuth property 추가

--- src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
+++ src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
@@ -1,13 +1,17 @@
 package univ.study.recruitjogbo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RecruitJogbo {
 
+    private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(RecruitJogbo.class, args);
+        new SpringApplicationBuilder(RecruitJogbo.class)
+                .properties(PROPERTIES)
+                .run(args);
     }
 
 }

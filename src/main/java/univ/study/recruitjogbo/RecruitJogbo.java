81c8ddc39092d14868632bcec5cc5a78dcdc21bf
hellozin <paul26375@gmail.com>
2019-07-20 06:26:28 +0900

    ADD: MailSender 서비스 추가

--- src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
+++ src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
@@ -6,11 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class RecruitJogbo {
 
-    private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+    private static final String GOOGLE_YML = "spring.config.location=classpath:/google.yml";
+    private static final String MAIL_YML = "spring.config.location=classpath:/mail.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(RecruitJogbo.class)
-                .properties(PROPERTIES)
+                .properties(GOOGLE_YML, MAIL_YML)
                 .run(args);
     }
 

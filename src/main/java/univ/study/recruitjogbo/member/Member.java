9e37b17da69654c57ebe63049e9cf27bc6f2c609
hellozin <paul26375@gmail.com>
2019-07-23 19:40:30 +0900

    UPDATE: Entity에 Validatation 제거

--- src/main/java/univ/study/recruitjogbo/member/Member.java
+++ src/main/java/univ/study/recruitjogbo/member/Member.java
@@ -1,13 +1,10 @@
 package univ.study.recruitjogbo.member;
 
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter
@@ -18,18 +15,12 @@ public class Member {
     @Id @GeneratedValue
     private Long seq;
 
-    @NotBlank
-    @Length(min = 4)
     private String userId;
 
-    @NotBlank
-    @Length(min = 4, max = 15)
     private String password;
 
-    @NotBlank
     private String name;
 
-    @Email
     private String email;
 
     @Builder
--- src/main/java/univ/study/recruitjogbo/post/Post.java
+++ src/main/java/univ/study/recruitjogbo/post/Post.java
@@ -4,8 +4,6 @@ import lombok.*;
 import univ.study.recruitjogbo.member.RecruitType;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -17,17 +15,13 @@ public class Post {
     @Id @GeneratedValue
     private Long seq;
 
-    @NotBlank
     private String companyName;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     private RecruitType recruitType;
 
-    @NotNull
     private LocalDate deadLine;
 
-    @NotBlank
     private String review;
 
     @Builder

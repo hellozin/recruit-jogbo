5c7e22946d41db21c85376580ec4df1a2cc06eae
hellozin <paul26375@gmail.com>
2019-07-31 21:10:40 +0900

    UPDATE: JWT 기반 인증 적용

--- src/main/java/univ/study/recruitjogbo/post/PostController.java
+++ src/main/java/univ/study/recruitjogbo/post/PostController.java
@@ -1,8 +1,10 @@
 package univ.study.recruitjogbo.post;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import univ.study.recruitjogbo.request.PostingRequest;
+import univ.study.recruitjogbo.security.JwtAuthentication;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,8 +17,10 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/content")
-    public Post publish(@RequestBody @Valid PostingRequest request) {
+    public Post publish(@AuthenticationPrincipal JwtAuthentication authentication,
+            @RequestBody @Valid PostingRequest request) {
         return postService.write(
+                authentication.id,
                 request.getCompanyName(),
                 request.getRecruitType(),
                 request.getDeadLine(),

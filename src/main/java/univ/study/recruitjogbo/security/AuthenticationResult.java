6fb4c996fff62987df3f213a3cab6ebfada8beb3
hellozin <paul26375@gmail.com>
2019-08-02 15:00:26 +0900

    UPDATE: 재사용을 위한 리팩토링

--- src/main/java/univ/study/recruitjogbo/security/AuthenticationResult.java
+++ src/main/java/univ/study/recruitjogbo/security/AuthenticationResult.java
@@ -1,19 +1,22 @@
 package univ.study.recruitjogbo.security;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import univ.study.recruitjogbo.member.Member;
 
 import static org.apache.commons.lang3.Validate.notBlank;
 import static org.apache.commons.lang3.Validate.notNull;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString
 public class AuthenticationResult {
 
-    private final String apiToken;
+    private String apiToken;
 
-    private final Member member;
+    private Member member;
 
     public AuthenticationResult(String apiToken, Member member) {
         notBlank(apiToken, "ApiToken must be provided.");

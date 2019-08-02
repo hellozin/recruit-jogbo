16b74bc1c3d4b39cc413d86f4b8f5855f70be37b
hellozin <paul26375@gmail.com>
2019-08-02 14:22:30 +0900

    FIX: MessageUtils 설정 변경

--- src/main/java/univ/study/recruitjogbo/configure/ServiceConfig.java
+++ src/main/java/univ/study/recruitjogbo/configure/ServiceConfig.java
@@ -2,9 +2,12 @@ package univ.study.recruitjogbo.configure;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
 import univ.study.recruitjogbo.security.JWT;
+import univ.study.recruitjogbo.util.MessageUtils;
 
 @Configuration
 @Slf4j
@@ -23,6 +26,16 @@ public class ServiceConfig {
     public JWT jwt() {
         log.info("Jwt Information issuer : {} clientSecret : {}", issuer, clientSecret);
         return new JWT(issuer, clientSecret, expirySeconds);
+    }
+
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
+        return new MessageSourceAccessor(messageSource);
+    }
+
+    @Bean
+    public MessageUtils messageUtils() {
+        return MessageUtils.getInstance();
     }
 
 }

44cb4cbe32dfdbeb95883028cecc9c843530148b
hellozin <paul26375@gmail.com>
2019-08-17 21:27:44 +0900

    UPDATE: 테스트를 위한 데이터 추가

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/RecruitJogboWeb.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/RecruitJogboWeb.java
@@ -39,7 +39,9 @@ public class RecruitJogboWeb {
         public void run(String... args) {
             Member member = memberService.join("paul", "1234", "hellozin", "hello@gmail.com");
             postService.write(member.getId(),"삼성", RecruitType.RESUME, LocalDate.now(), "이러저러했다.");
-            postService.write(member.getId(),"라인", RecruitType.INTERVIEW, LocalDate.now(), "저러이러했다.");
+            postService.write(member.getId(),"삼성", RecruitType.INTERVIEW, LocalDate.now(), "이러저러했다.");
+            postService.write(member.getId(),"라인", RecruitType.INTERVIEW, LocalDate.now().minusDays(10), "저러이러했다.");
+            postService.write(member.getId(),"라인", RecruitType.CODING, LocalDate.now().minusDays(2), "저러이러했다.");
         }
     }
 

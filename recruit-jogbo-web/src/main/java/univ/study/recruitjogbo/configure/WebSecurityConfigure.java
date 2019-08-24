4997d0afb5d70caec517ef93c7ee33457767b2c8
hellozin <paul26375@gmail.com>
2019-08-25 01:45:38 +0900

    UPDATE: 회원가입 페이지 권한 해제

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
@@ -31,6 +31,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/member/new").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()

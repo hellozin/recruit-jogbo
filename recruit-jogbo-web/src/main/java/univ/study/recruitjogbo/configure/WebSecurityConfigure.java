8d06c7fa29c10eef79edd41be625560e015f204a
hellozin <paul26375@gmail.com>
2019-08-24 20:21:20 +0900

    UPDATE: 토큰 기반 인증에서 FormLogin 기반 인증으로 변경

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/configure/WebSecurityConfigure.java
@@ -1,6 +1,6 @@
 package univ.study.recruitjogbo.configure;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,16 +9,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import univ.study.recruitjogbo.member.MemberService;
-import univ.study.recruitjogbo.security.JWT;
-import univ.study.recruitjogbo.security.JwtAuthenticationProvider;
-import univ.study.recruitjogbo.security.JwtAuthenticationTokenFilter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     @Override
@@ -26,42 +27,29 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public JwtAuthenticationProvider jwtAuthenticationProvider(JWT jwt, MemberService memberService) {
-        return new JwtAuthenticationProvider(jwt, memberService);
-    }
-
-    @Bean
-    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
-        return new JwtAuthenticationTokenFilter();
-    }
-
-    @Autowired
-    public void configureAuthentication(AuthenticationManagerBuilder builder, JwtAuthenticationProvider authenticationProvider) {
-        builder.authenticationProvider(authenticationProvider);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                    .disable()
-                .headers()
-                    .disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
                 .authorizeRequests()
-                    .anyRequest().permitAll()
+                    .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .disable();
-        http
-                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/post/list")
+                    .permitAll()
+                    .and()
+                .logout();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/h2/**");
+        web.ignoring().antMatchers("/h2/**", "/templates/**");
     }
+
 }
---
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package univ.study.recruitjogbo.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import univ.study.recruitjogbo.member.Member;
+import univ.study.recruitjogbo.member.MemberService;
+import univ.study.recruitjogbo.member.Role;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberService memberService;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Member member = memberService.findByMemberId(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException(memberId));
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        if (member.isEmailConfirmed()) {
+            authorities.add(new SimpleGrantedAuthority(Role.MEMBER.value()));
+        } else {
+            authorities.add(new SimpleGrantedAuthority(Role.UNCONFIRMED.value()));
+        }
+
+        return new User(member.getMemberId(), member.getPassword(), authorities);
+    }
+
+}

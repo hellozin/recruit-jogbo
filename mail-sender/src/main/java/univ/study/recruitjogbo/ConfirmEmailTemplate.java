3510c716b4440f94b82e955fd56a5f448d39cb2a
hellozin <paul26375@gmail.com>
2019-08-28 19:14:02 +0900

    UPDATE: 문자열 관리 인터페이스로 변경

--- mail-sender/src/main/java/univ/study/recruitjogbo/ConfirmEmailTemplate.java
+++ mail-sender/src/main/java/univ/study/recruitjogbo/ConfirmEmailTemplate.java
@@ -1,13 +1,10 @@
 package univ.study.recruitjogbo;
 
-import lombok.Getter;
+public interface ConfirmEmailTemplate {
 
-@Getter
-public class ConfirmEmailTemplate {
+    String subject = "[Recruit Jogbo] 사용자 인증 메일입니다.";
 
-    static final String subject = "[Recruit Jogbo] 사용자 인증 메일입니다.";
-
-    static final String text = "이메일을 인증하기 위해 다음 링크를 클릭해 주세요.\n 링크: " +
+    String text = "이메일을 인증하기 위해 다음 링크를 클릭해 주세요.\n 링크: " +
             "http://localhost:8080/api/confirm/email?token=";
 
 }

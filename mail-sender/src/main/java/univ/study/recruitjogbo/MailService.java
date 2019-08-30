7d1290ddc6794095aa6c734a3b8b17994ddeb82d
hellozin <paul26375@gmail.com>
2019-08-31 00:21:19 +0900

    REFACTOR: 필요한 필드만 생성자 생성

--- mail-sender/src/main/java/univ/study/recruitjogbo/MailService.java
+++ mail-sender/src/main/java/univ/study/recruitjogbo/MailService.java
@@ -1,12 +1,12 @@
 package univ.study.recruitjogbo;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MailService {
 
     private final JavaMailSender mailSender;

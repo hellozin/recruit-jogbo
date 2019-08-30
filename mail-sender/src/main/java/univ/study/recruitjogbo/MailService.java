4a1a78e36a9f61db5967592a4721fe415a53aaa3
hellozin <paul26375@gmail.com>
2019-08-31 03:56:50 +0900

    UPDATE: 가입 인증 메일 Thymeleaf로 관리
    resolve #13

--- mail-sender/src/main/java/univ/study/recruitjogbo/MailService.java
+++ mail-sender/src/main/java/univ/study/recruitjogbo/MailService.java
@@ -1,9 +1,14 @@
 package univ.study.recruitjogbo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -11,12 +16,20 @@ public class MailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+    private final TemplateEngine templateEngine;
+
+    public void sendConfirmMail(String to, String subject, String token) throws MessagingException {
+        Context context = new Context();
+        context.setVariable("token", token);
+        String htmlContent = templateEngine.process("mailContent", context);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true);
+
+        mailSender.send(mimeMessage);
     }
 
 }

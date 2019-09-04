4db774fddc3925e9ef5aa4946ecc1e21eb170334
hellozin <paul26375@gmail.com>
2019-09-05 02:00:53 +0900

    UPDATE: 메일 인증 관련 Controller 수정

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/controller/MemberController.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/controller/MemberController.java
@@ -1,12 +1,17 @@
 package univ.study.recruitjogbo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import univ.study.recruitjogbo.error.NotFoundException;
+import univ.study.recruitjogbo.member.Member;
 import univ.study.recruitjogbo.member.MemberService;
 import univ.study.recruitjogbo.request.JoinRequest;
+import univ.study.recruitjogbo.security.AuthMember;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -34,6 +39,16 @@ public class MemberController {
                 request.getPassword(),
                 request.getEmail()
         );
+        return "redirect:/login";
+    }
+
+    @PostMapping("/member/confirm")
+    public String emailConfirm(@AuthenticationPrincipal AuthMember member) {
+        String email = memberService.findById(member.getId())
+                .map(Member::getEmail)
+                .orElseThrow(() -> new NotFoundException(Member.class, member.getId().toString()));
+        memberService.sendConfirmEmail(email);
+        SecurityContextHolder.clearContext();
         return "redirect:/login";
     }
 

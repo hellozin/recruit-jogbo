0351ed9fab57f3795edc2aeffbc46a5f2d3d19a9
hellozin <paul26375@gmail.com>
2019-09-05 02:00:53 +0900

    UPDATE: 메일 인증 관련 Controller 수정

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/controller/AuthenticationController.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/controller/AuthenticationController.java
@@ -3,14 +3,27 @@ package univ.study.recruitjogbo.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import univ.study.recruitjogbo.member.MemberService;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final MemberService memberService;
+
     @GetMapping("/login")
     public String loginForm() {
         return "login";
+    }
+
+    @GetMapping("/confirm")
+    public String confirmForm() {
+        return "confirm";
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
     }
 
 }

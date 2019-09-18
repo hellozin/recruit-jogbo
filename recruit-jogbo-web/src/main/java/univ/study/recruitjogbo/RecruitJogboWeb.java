f74327816d809e6d5116a01e524fcf83e86e805d
hellozin <paul26375@gmail.com>
2019-09-19 01:21:15 +0900

    UPDATE: 테스트용 admin 계정 추가

--- recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/RecruitJogboWeb.java
+++ recruit-jogbo-web/src/main/java/univ/study/recruitjogbo/RecruitJogboWeb.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Component;
+import univ.study.recruitjogbo.member.Member;
+import univ.study.recruitjogbo.member.MemberService;
 import univ.study.recruitjogbo.post.RecruitType;
 import univ.study.recruitjogbo.post.RecruitTypeRepository;
 import univ.study.recruitjogbo.post.RecruitTypes;
@@ -30,11 +32,17 @@ public class RecruitJogboWeb {
 
         private final RecruitTypeRepository recruitTypeRepository;
 
+        private final MemberService memberService;
+
         @Override
         public void run(String... args) {
             for (RecruitTypes recruitType : RecruitTypes.values()) {
                 recruitTypeRepository.save(new RecruitType(recruitType));
             }
+
+            Member member = memberService.join("admin", "admin", "paul5813@ynu.ac.kr");
+            member.setEmailConfirmed(true);
+            memberService.save(member);
         }
     }
 

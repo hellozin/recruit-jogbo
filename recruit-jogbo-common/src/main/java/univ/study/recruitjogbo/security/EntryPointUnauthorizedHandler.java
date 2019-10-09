c31d10326f888a63e137aeb30473f7db6d5995b1
hellozin <paul26375@gmail.com>
2019-10-09 23:46:59 +0900

    UPDATE: 인증되지 않은 사용자, 권한이 없는 경우 예외처리

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/security/EntryPointUnauthorizedHandler.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/security/EntryPointUnauthorizedHandler.java
@@ -1,19 +1,32 @@
 package univ.study.recruitjogbo.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import univ.study.recruitjogbo.api.response.ApiError;
+import univ.study.recruitjogbo.api.response.ApiResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class EntryPointUnauthorizedHandler implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        System.out.println("commence");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("content-type", "application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ApiResponse.ERROR(new ApiError(authException.getMessage(), HttpStatus.UNAUTHORIZED))));
+        response.getWriter().flush();
+        response.getWriter().close();
     }
 
 }

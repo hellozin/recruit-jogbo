183b4fe1a0693af8358ae1dd1dd13d5baf407834
hellozin <paul26375@gmail.com>
2019-10-12 05:25:07 +0900

    FRONTEND: 불필요한 기본 코드 제거

--- recruit-jogbo-common/frontend/src/components/HelloWorld.vue
+++ recruit-jogbo-common/frontend/src/components/HelloWorld.vue
@@ -8,12 +8,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>

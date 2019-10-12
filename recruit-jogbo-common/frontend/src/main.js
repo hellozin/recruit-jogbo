aff799e4819b9bcecd58ac5d762f6624912cfe32
hellozin <paul26375@gmail.com>
2019-10-12 11:39:32 +0900

    FRONTEND: Http 요청을 위한 Axios 의존성 추가

--- recruit-jogbo-common/frontend/src/main.js
+++ recruit-jogbo-common/frontend/src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

4b0027d5e5898a44d3c6dabcc63dac9352e29f13
hellozin <paul26375@gmail.com>
2019-10-13 17:03:19 +0900

    FRONTEND: 로그인, Post 조회, Post 목록 조회 라우팅 추가

--- recruit-jogbo-common/frontend/src/router/index.js
+++ recruit-jogbo-common/frontend/src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import SignIn from '@/components/SignIn'
+import PostList from '@/components/PostList'
+import Post from '@/components/Post'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
+    }, {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'PostList',
+      component: PostList
+    }, {
+      path: '/post/:id',
+      name: 'Post',
+      component: Post
     }
   ]
 })

b1bec5cd27a9ab1c9696e6b26b458c581f394410
hellozin <paul26375@gmail.com>
2019-10-19 20:05:56 +0900

    FRONTEND: 모든 request에 apiToken 추가, error 전역 handler 추가

--- recruit-jogbo-common/frontend/src/main.js
+++ recruit-jogbo-common/frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import EventBus from './EventBus'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,14 +12,37 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 const instance = axios.create({
-  headers: {
-    common: {
-      api_key: 'Bearer ' + localStorage.getItem('apiToken')
-    }
-  }
 })
 
 instance.defaults.headers['Content-Type'] = 'application/json'
+
+instance.interceptors.request.use(
+  function (config) {
+    const apiToken = localStorage.getItem('apiToken')
+    if (apiToken) {
+      config.headers['api_key'] = 'Bearer ' + apiToken
+    }
+    return config
+  }, function (error) {
+    Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use(
+  function (response) {
+    return response
+  }, function (error) {
+    if (error.response) {
+      console.log('error response')
+      const response = error.response.data.response
+      if (response.status === 'UNAUTHORIZED') {
+        localStorage.removeItem('apiToken')
+        window.location.href = '#/login'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 Vue.prototype.$axios = instance
 

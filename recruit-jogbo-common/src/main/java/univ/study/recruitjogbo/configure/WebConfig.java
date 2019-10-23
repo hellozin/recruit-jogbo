62412c0f59ef30326058427ce90a97c8f3bee16f
hellozin <paul26375@gmail.com>
2019-10-23 21:01:17 +0900

    UPDATE: CORS allow method에 PUT, DELETE 추가

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
@@ -1,6 +1,7 @@
 package univ.study.recruitjogbo.configure;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081");
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 
 }

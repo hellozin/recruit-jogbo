66067ba7f9a6570ac4f3c12cea13a887d91938a9
hellozin <paul26375@gmail.com>
2019-11-02 18:17:33 +0900

    UPDATE: 테스트용 CommandLineRunner 제거

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/RecruitJogbo.java
@@ -1,22 +1,7 @@
 package univ.study.recruitjogbo;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import univ.study.recruitjogbo.member.Member;
-import univ.study.recruitjogbo.member.MemberRepository;
-import univ.study.recruitjogbo.review.Review;
-import univ.study.recruitjogbo.review.ReviewRepository;
-import univ.study.recruitjogbo.review.recruitType.RecruitType;
-import univ.study.recruitjogbo.review.recruitType.RecruitTypeEntity;
-import univ.study.recruitjogbo.review.recruitType.RecruitTypeRepository;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class RecruitJogbo {
@@ -31,42 +16,6 @@ public class RecruitJogbo {
         new SpringApplicationBuilder(RecruitJogbo.class)
                 .properties(PROPERTIES)
                 .run(args);
-    }
-
-    @Component
-    @RequiredArgsConstructor
-    class Runner implements CommandLineRunner {
-
-        private final MemberRepository memberRepository;
-
-        private final ReviewRepository reviewRepository;
-
-        private final RecruitTypeRepository recruitTypeRepository;
-
-        private final PasswordEncoder passwordEncoder;
-
-        @Override
-        public void run(String... args) {
-            for (RecruitType recruitType : RecruitType.values()) {
-                recruitTypeRepository.save(new RecruitTypeEntity(recruitType));
-            }
-
-            Member member = new Member("admin", passwordEncoder.encode("admin"), "admin@ynu.ac.kr");
-            memberRepository.save(member);
-
-            LocalDate today = LocalDate.now();
-            for (int i = 0; i < 10; i++) {
-                List<RecruitTypeEntity> byRecruitTypeEntityIn = recruitTypeRepository.findByRecruitTypeIn(Arrays.asList(RecruitType.RESUME, RecruitType.APTITUDE));
-                Review review = new Review(
-                        member,
-                        "company" + i,
-                        "detail" + i,
-                        byRecruitTypeEntityIn,
-                        today.minusDays(i),
-                        "review" + i);
-                reviewRepository.save(review);
-            }
-        }
     }
 
 }

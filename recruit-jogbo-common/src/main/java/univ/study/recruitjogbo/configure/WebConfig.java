1633dcf0bd0a5868d02e7ba6f83ba8a5cf08dbaf
hellozin <paul26375@gmail.com>
2019-11-02 21:08:10 +0900

    ADD: Frontend 서버 도메인 변경

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://recruit-jogbo.tk")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),

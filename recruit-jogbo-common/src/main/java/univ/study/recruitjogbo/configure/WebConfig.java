810c728f93d50f90d6247b73fe73e174422d37ab
hellozin <paul26375@gmail.com>
2019-11-02 21:12:54 +0900

    FIX: prefix 추가

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://recruit-jogbo.tk")
+                .allowedOrigins("http://www.recruit-jogbo.tk")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),

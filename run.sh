79b1072d572e13398f3879910178cc07f5228568
hellozin <paul26375@gmail.com>
2019-11-04 01:18:12 +0900

    ADD: 프로젝트 실행 스크립트 추가

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# git pull
+git pull origin master
+
+# build bootjar
+./gradlew bootjar
+
+# docker build Recruit Jogbo API Server
+docker build -t recruit-jogbo ./recruit-jogbo-common/
+
+# docker build Recruit Jogbo Mail Server
+docker build -t recruit-jogbo-mail ./mail-sender/
+
+# docker run with compose
+docker-compose up
+

49801ee36e1ba38053178c0e3b3ae6a26d481906
hellozin <paul26375@gmail.com>
2019-11-04 01:08:30 +0900

    UPDATE: local profile 시 Cors Origin 모두 허용

--- recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
+++ recruit-jogbo-common/src/main/java/univ/study/recruitjogbo/configure/WebConfig.java
@@ -1,15 +1,23 @@
 package univ.study.recruitjogbo.configure;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final Environment environment;
+
+    private static String frontEndOrigin = "http://www.recruit-jogbo.tk";
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -18,8 +26,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        if (Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+            frontEndOrigin = "*";
+        }
         registry.addMapping("/**")
-                .allowedOrigins("http://www.recruit-jogbo.tk")
+                .allowedOrigins(frontEndOrigin)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
